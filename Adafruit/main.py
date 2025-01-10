43aa29ec0385eec1b51fe17f417785b5fd564549
VogtD01 <vd6806@mci4me.at>
2025-01-10 13:58:31 +0100

    led

--- Adafruit/main.py
+++ Adafruit/main.py
@@ -6,7 +6,7 @@ from ADXL345 import ADXL345_I2C
 #Lora
 led = Pin(2)
 CS = Pin(5, Pin.OUT)
-RESET = Pin(34, Pin.OUT)
+RESET = Pin(27, Pin.OUT)
 spi = SPI(2, baudrate=1000000, polarity=0, phase=0, bits=8, firstbit=0, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
 RADIO_FREQ_MHZ = 433.0
 # Initialze RFM radio
@@ -49,14 +49,13 @@ while True:
     # packet = rfm9x.receive(timeout=5.0)
     # If no packet was received during the timeout then None is returned.
 
-    
     if packet is None:
         # Packet has not been received
-        led.value(0)
+        green.value(0)
         print("Received nothing! Listening again...")
     else:
         # Received a packet!
-        led.value(1)
+        green.value(1)
         # Print out the raw bytes of the packet:
         print("Received (raw bytes): {0}".format(packet))
         # And decode to ASCII text and print it too.  Note that you always
@@ -65,6 +64,13 @@ while True:
         # sending side is sending ASCII data before you try to decode!
         packet_text = str(packet, "ascii")
         print("Received (ASCII): {0}".format(packet_text))
+        
+        # Check if the received message is "Button gedrückt"
+        if packet_text == "Button gedrückt!\r\n":
+            red.value(1)  # Turn on the red LED
+        else:
+            red.value(0)  # Turn off the red LED
+        
         # Also read the RSSI (signal strength) of the last received message and
         # print it.
         rssi = rfm9x.last_rssi
