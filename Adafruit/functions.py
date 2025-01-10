8700b34095e943ff2b18727a925ddf776a704410
VogtD01 <vd6806@mci4me.at>
2025-01-10 18:56:37 +0100

    imu funktioniert

--- Adafruit/functions.py
+++ Adafruit/functions.py
@@ -1,6 +1,18 @@
 import math
+import time
+from machine import Pin, PWM
+
 
 
 def calculate_magnitude(x, y, z):
     """Berechnet die Magnitude des Beschleunigungsvektors."""
     return math.sqrt(x**2 + y**2 + z**2)
+
+def button_nachricht():
+    red.value(1)  # Rote LED einschalten
+    green.value(1)  # Grüne LED einschalten
+    piezo_pin.duty(512)  # Piezo aktivieren
+    time.sleep(1)
+    red.value(0)  # Rote LED ausschalten
+    green.value(0)  # Grüne LED ausschalten
+    piezo_pin.duty(0)  # Piezo deaktivieren
--- Adafruit/main.py
+++ Adafruit/main.py
@@ -2,6 +2,7 @@ from adafruit_rfm9x import *
 from machine import SPI, Pin, PWM, I2C
 import time
 from ADXL345 import ADXL345_I2C
+import math
 
 # Lora
 led = Pin(2)
@@ -16,6 +17,17 @@ rfm9x.tx_power = 23
 # IMU
 i2c = I2C(0, scl=Pin(22), sda=Pin(21))
 imu = ADXL345_I2C(i2c)
+
+def calculate_magnitude(x, y, z):
+    """Berechnet die Magnitude des Beschleunigungsvektors."""
+    return math.sqrt(x**2 + y**2 + z**2)
+
+# Schwellenwert für freien Fall (nahe 0 g in allen Achsen)
+FALL_THRESHOLD = 0.35  # g-Wert, anpassbar
+
+# Variable, um zu speichern, ob ein freier Fall erkannt wurde
+fall_detected = False
+
 
 # Piezo-speaker
 piezo_pin = PWM(Pin(15))
@@ -34,9 +46,9 @@ button = Pin(14, Pin.IN, Pin.PULL_UP)
 
 # Interrupt handler for button press
 def button_pressed_handler(pin):
-    rfm9x.send(bytes("Button gedrückt!\r\n", "utf-8"))
+    # Nachricht senden: "001" für "Button gedrückt"
+    rfm9x.send(bytes([0b001]))
     print("Button gedrückt Nachricht gesendet!")
-
 
 # Set up the button interrupt
 button.irq(trigger=Pin.IRQ_FALLING, handler=button_pressed_handler)
@@ -54,32 +66,71 @@ piezo_pin.duty(0)
 blue.value(0)
 red.value(0)
 
+
+
+###################################
+
+def button_nachricht():
+    red.value(1)  # Rote LED einschalten
+    green.value(1)  # Grüne LED einschalten
+    piezo_pin.duty(512)  # Piezo aktivieren
+    time.sleep(1)
+    red.value(0)  # Rote LED ausschalten
+    green.value(0)  # Grüne LED ausschalten
+    piezo_pin.duty(0)  # Piezo deaktivieren
+
+def fall_nachricht():
+
+    red.value(1)  # Rote LED einschalten
+    blue.value(1)  # Blaue LED einschalten
+    piezo_pin.duty(512)  # Piezo aktivieren
+    time.sleep(5)
+    red.value(0)  # Rote LED ausschalten
+    blue.value(0)  # Blaue LED ausschalten
+    piezo_pin.duty(0)  # Piezo deaktivieren
+###############################
+
 while True:
+    # Empfange ein Paket mit den RFM9x-Funkmodul
     packet = rfm9x.receive()
-    # Optionally change the receive timeout from its default of 0.5 seconds:
-    # packet = rfm9x.receive(timeout=5.0)
-    # If no packet was received during the timeout, then None is returned.
+
+    # Lese die Beschleunigungswerte
+    x = imu.xValue / 256  # Werte in g umwandeln (Division anpassen, falls nötig)
+    y = imu.yValue / 256
+    z = imu.zValue / 256
+
+    # Berechne die Magnitude (Betrag des Beschleunigungsvektors)
+    magnitude = calculate_magnitude(x, y, z)
+
+    # Prüfe auf freien Fall anhand eines Schwellenwerts
+    if magnitude < FALL_THRESHOLD:
+        fall_detected = True
+        blue.value(1)  # Blaue LED einschalten
+        time.sleep(5)  # LED für 5 Sekunden anlassen
+        blue.value(0)  # Blaue LED ausschalten
+
+        # Nachricht senden: "100" für freien Fall erkannt
+        rfm9x.send(bytes([0b100]))
+        print("Fall detected Nachricht gesendet!")
 
     if packet is None:
-        # Packet has not been received
+        # Kein Paket empfangen
         print("Received nothing! Listening again...")
     else:
-        # Received a packet!
+        # Ein Paket wurde empfangen
         print("Received (raw bytes): {0}".format(packet))
-        # Decode to ASCII text and print it
-        packet_text = str(packet, "ascii")
-        print("Received (ASCII): {0}".format(packet_text))
 
-        # Check if the received message is "Button gedrückt"
-        if packet_text == "Button gedrückt!\r\n":
-            red.value(1)  # Turn on the red LED
-            green.value(1)  # Turn on the green LED
-            piezo_pin.duty(512)
-            time.sleep(1)
-            red.value(0)  # Turn off the red LED
-            green.value(0)  # Turn off the green LED
-            piezo_pin.duty(0)
+        # Paketinhalte analysieren (binäre Zustände)
+        if packet[0] == 0b001:  # "001" entspricht "Button gedrückt"
+            
+            button_nachricht()
+        
 
-        # Read and print the RSSI (signal strength) of the last received message
+        elif packet[0] == 0b100:  # "100" entspricht "Fall erkannt"
+
+            fall_nachricht()
+
+
+        # Signalstärke (RSSI) des empfangenen Pakets auslesen und ausgeben
         rssi = rfm9x.last_rssi
         print("Received signal strength: {0} dB".format(rssi))
